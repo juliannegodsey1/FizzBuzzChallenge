6c7189c75eaa12083b8716caf83872d322fca8c1
juliannegodsey1 <juliannegodseyy@gmail.com>
2023-10-16 13:19:06 -0400

    initial commit

---
+++ FizzBuzzChallenge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+public string FizzBuzz(int number)
+{
+    if(number % 3 == 0)
+    {
+        Console.WriteLine("fizz");
+    }
+    if (number % 5 == 0)
+    {
+        Console.WriteLine("buzz");
+    }
+    if (number % 3 == 0 && number % 5 ==0)
+    {
+        Console.WriteLine("fizzbuzz");
+    }
+}
