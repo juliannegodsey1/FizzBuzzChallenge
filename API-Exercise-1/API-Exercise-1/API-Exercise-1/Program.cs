dc90cd0c06461a979f8fac0e0b22f8d9890c1398
juliannegodsey1 <juliannegodseyy@gmail.com>
2023-10-20 14:05:33 -0400

    initial commit

---
+++ API-Exercise-1/API-Exercise-1/API-Exercise-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace API_Exercise_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+
+        }
+
+    }
+}
